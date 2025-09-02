b0c0550679c0f80f34732ea91f06faa61804cdfe
Harshit <harshitmishragarhwa@gmail.com>
2025-09-02 07:56:31 +0530

    Api Handling Implemented

---
+++ app/src/main/java/com/example/climacast/MainActivity2.kt
@@ -0,0 +1,53 @@
+package com.example.climacast
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.climacast.databinding.ActivityMain2Binding
+import com.example.climacast.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    private val apiKey = "e6bd8dabc0ea5a62c9e4098f901eef46"
+    private lateinit var weatherService: WeatherService
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        val cityName = intent.getStringExtra("CityName")
+        weatherService = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherService::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val weatherData = weatherService.getWeather(cityName.toString(), apiKey)
+            withContext(Dispatchers.Main) {
+                updateUI(weatherData)
+            }
+        }
+    }
+
+    private fun updateUI(weatherData: WeatherData) {
+        binding.textViewCity.text = weatherData.name
+        binding.textViewTemperature.text =
+            "${weatherData.main.temp.toInt()}°C"
+        val iconUrl = "https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png"
+        Glide.with(this)
+            .load(iconUrl)
+            .into(binding.imageViewWeatherIcon)
+    }
+
+}
